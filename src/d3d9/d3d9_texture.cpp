2281fa959650502c074ba36cd50a3aa7c8230978
Quepas <oneinchman.dev@gmail.com>
2014-02-06 14:53:35 +0100

    Adding d3d9::Texture class.

---
+++ src/d3d9/d3d9_texture.cpp
@@ -0,0 +1,25 @@
+#include "d3d9_texture.hpp"
+
+#include <d3dx9.h>
+
+namespace aff {
+  namespace d3d9 {
+
+Texture::Texture(std::string file_path, IDirect3DDevice9* d3d9_device)
+  : d3d9_device_(d3d9_device), 
+    d3d9_texture_(nullptr), 
+    logger_("d3d9::Texture")
+{
+  D3DXCreateTextureFromFile(
+    d3d9_device_, file_path.c_str(), &d3d9_texture_);
+  if (!d3d9_texture_)
+    logger_.Error("Loading texture failed: " + file_path);
+}
+
+Texture::~Texture()
+{
+  d3d9_texture_->Release();
+}
+
+// ~~ aff::d3d9::Texture
+}}
---
+++ src/d3d9/d3d9_texture.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <d3d9.h>
+#include <string>
+
+#include "../util/util_logger.hpp"
+
+namespace aff {
+  namespace d3d9 {
+
+class Texture 
+{
+public:
+  Texture(std::string file_path, IDirect3DDevice9* d3d9_device);
+  ~Texture();
+
+  inline IDirect3DTexture9* d3d9_texture();
+  inline operator IDirect3DTexture9*();
+private:
+  IDirect3DTexture9* d3d9_texture_;
+  IDirect3DDevice9* d3d9_device_;
+  util::Logger logger_;
+
+  // for now...
+  Texture(const Texture&);
+  Texture& operator=(const Texture&);
+};
+
+inline IDirect3DTexture9* Texture::d3d9_texture() {
+  return d3d9_texture_;
+}
+
+inline Texture::operator IDirect3DTexture9*() {
+  return d3d9_texture_;
+}
+
+// ~~ aff::d3d9::Texture
+}}
