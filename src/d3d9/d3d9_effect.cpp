3be1e6a9ce78f2fe38ea04fc80aab09415f2273a
Quepas <oneinchman.dev@gmail.com>
2014-02-07 03:13:12 +0100

    Print errors occured in effect file.

--- src/d3d9/d3d9_effect.cpp
+++ src/d3d9/d3d9_effect.cpp
@@ -14,15 +14,11 @@ Effect::Effect(string file_path, IDirect3DDevice9* d3d9_device)
   D3DXCreateEffectFromFile(
     d3d9_device_, file_path.c_str(), NULL, 
     NULL, 0, NULL, &d3d9_effect_, &error_buffer);
-  if (!d3d9_effect_) 
-  {
+  if (!d3d9_effect_)   
     logger_.Error("Loading shader failed: " + file_path);
-    if (error_buffer)
-    {
-      logger_.Error(string("\tBecause: ") 
-        + static_cast<char*>(error_buffer->GetBufferPointer()));
-    }
-  }
+  if (error_buffer)
+    logger_.Error(string("\tLoading errors: ")
+      + static_cast<char*>(error_buffer->GetBufferPointer()));  
 }
 
 Effect::~Effect()
