b8b5fa680785175681f4e8f20e93e97a50c596a0
Quepas <oneinchman.dev@gmail.com>
2014-08-06 01:41:29 +0200

    Functions for converting in/to radians/degrees. PI as constant. Function for converting from aff to d3d9 3d vector.

--- Engine/src/math_util.cpp
+++ Engine/src/math_util.cpp
@@ -10,5 +10,15 @@ bool AlmostEqual(float a, float b, float eps /*= 0.00001f*/)
   return abs(a - b) < eps;
 }
 
+float Rad2Deg(float rad)
+{
+  return rad * (180.0f / PIf);
+}
+
+float Deg2Rad(float deg)
+{
+  return deg * (PIf / 180.0f);
+}
+
 }}
 // ~~ aff::math
--- Engine/src/math_util.hpp
+++ Engine/src/math_util.hpp
@@ -1,11 +1,28 @@
 #pragma once
 
+#include "math_vector3.hpp"
+
 #include <cmath>
+#include <d3dx9.h>
 
 namespace aff {
   namespace math {
 
+const double PI = 3.14159265358979323846;
+const float PIf = static_cast<float>(PI);
+
 bool AlmostEqual(float a, float b, float eps = 0.00001f);
+float Rad2Deg(float rad);
+float Deg2Rad(float deg);
+template<typename T>
+D3DXVECTOR3 ToD3DXVector3(const Vector3<T>& vector);
 
 }}
+
+template<typename T>
+D3DXVECTOR3 aff::math::ToD3DXVector3(const Vector3<T>& vector)
+{
+  return D3DXVECTOR3(vector.x, vector.y, vector.z);
+}
+
 // ~~ aff::math
