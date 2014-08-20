698f1a614f4badf21c335f8ecfd533c29758e398
Quepas <oneinchman.dev@gmail.com>
2014-08-20 23:11:40 +0200

    Camera is now SceneEntity.

--- Engine/src/core_camera.cpp
+++ Engine/src/core_camera.cpp
@@ -1,5 +1,7 @@
 #include "core_camera.hpp"
 
+using aff::math::Vector3f;
+
 namespace aff {
   namespace core {
 
@@ -8,10 +10,9 @@ Camera::Camera()
     near_clipping_plane_(1.0f),
     far_clipping_plane_(100.0f),
     view_ratio_(4.0f / 3.0f),
-    position_(0.0f, 0.0f, -3.0f),
     look_at_(0.0f, 0.0f, 0.0f)
 {
-
+  MoveTo(Vector3f(0.0f, 0.0f, -3.0f));
 }
 
 Camera::Camera(const math::Vector3f& position, const math::Vector3f& look_at, float field_of_view)
@@ -19,20 +20,14 @@ Camera::Camera(const math::Vector3f& position, const math::Vector3f& look_at, fl
     near_clipping_plane_(1.0f),
     far_clipping_plane_(100.0f),
     view_ratio_(4.0f / 3.0f),
-    position_(position),
     look_at_(look_at)
 {
-
+  MoveTo(Vector3f(0.0f, 0.0f, -3.0f));
 }
 
 Camera::~Camera()
 {
 
-}
-
-void Camera::MoveTo(const math::Vector3f& position)
-{
-  position_ = position;
 }
 
 void Camera::LookAt(const math::Vector3f& look_at)
--- Engine/src/core_camera.hpp
+++ Engine/src/core_camera.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include "core_scene_entity.hpp"
 #include "math_vector3.hpp"
 
 namespace aff {
   namespace core {
 
-class Camera
+class Camera : public SceneEntity
 {
 public:
   Camera();
@@ -16,10 +17,8 @@ public:
   float near_clipping_plane() const { return near_clipping_plane_; }
   float far_clipping_plane() const { return far_clipping_plane_; }
   float view_ratio() const { return view_ratio_; }
-  math::Vector3f position() const { return position_; }
   math::Vector3f look_at() const { return look_at_; }
 
-  void MoveTo(const math::Vector3f& position);
   void LookAt(const math::Vector3f& look_at);
 
   void SetFieldOfView(float fov);
@@ -32,7 +31,6 @@ protected:
   float far_clipping_plane_;
   float view_ratio_;
 
-  math::Vector3f position_;
   math::Vector3f look_at_;
 };
 
